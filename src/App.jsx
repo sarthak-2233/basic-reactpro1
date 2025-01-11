0305ddf4d02e4b8153250293350bd0832308eb42
Sarthak Sharma <sarthak.sharma33333@gmail.com>
2025-01-12 00:59:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Hero from './components/Hero';
-import Navigation from './components/navigation';
+import Navigation from './components/Navigation';
 
 const App = () =>{
 
