978f892ad7201006411e3ca0db248944d12bb2c0
sarthak-2233 <sarthak.sharma33333@gmail.com>
2025-01-12 00:31:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Hero from './components/Hero';
+import Navigation from './components/navigation';
+
+const App = () =>{
+
+  return (
+    <div>
+  <Navigation/>
+  <Hero/>
+  
+  </div>
+  );
+};
+
+export default App;
